a58490bd54ba2a0546788b1030c27647873c7ffc
eventual-recluse <48379405+eventual-recluse@users.noreply.github.com>
2022-03-11 20:54:33 -0500

    Update and rename AppConfig.h to JucePluginCharacteristics.h

--- ports-legacy/argotlunar/JuceLibraryCode/JucePluginCharacteristics.h
+++ ports-legacy/argotlunar/JuceLibraryCode/JucePluginCharacteristics.h
@@ -15,166 +15,8 @@
 #define __JUCE_APPCONFIG_ULIIGD__
 
 //==============================================================================
-// [BEGIN_USER_CODE_SECTION]
-
-// (You can add your own code in this section, and the Introjucer will not overwrite it)
-
-// [END_USER_CODE_SECTION]
-
-//==============================================================================
-#define JUCE_MODULE_AVAILABLE_juce_audio_basics             1
-#define JUCE_MODULE_AVAILABLE_juce_audio_devices            1
-#define JUCE_MODULE_AVAILABLE_juce_audio_formats            1
-#define JUCE_MODULE_AVAILABLE_juce_audio_plugin_client      1
-#define JUCE_MODULE_AVAILABLE_juce_audio_processors         1
-#define JUCE_MODULE_AVAILABLE_juce_core                     1
-#define JUCE_MODULE_AVAILABLE_juce_data_structures          1
-#define JUCE_MODULE_AVAILABLE_juce_events                   1
-#define JUCE_MODULE_AVAILABLE_juce_graphics                 1
-#define JUCE_MODULE_AVAILABLE_juce_gui_basics               1
-#define JUCE_MODULE_AVAILABLE_juce_gui_extra                1
-
-//==============================================================================
-// juce_audio_devices flags:
-
-#ifndef    JUCE_ASIO
- #define   JUCE_ASIO 0
-#endif
-
-#ifndef    JUCE_WASAPI
- #define   JUCE_WASAPI 0
-#endif
-
-#ifndef    JUCE_DIRECTSOUND
- #define   JUCE_DIRECTSOUND 0
-#endif
-
-#ifndef    JUCE_ALSA
- #define   JUCE_ALSA 0
-#endif
-
-#ifndef    JUCE_JACK
- //#define JUCE_JACK
-#endif
-
-#ifndef    JUCE_USE_ANDROID_OPENSLES
- //#define JUCE_USE_ANDROID_OPENSLES
-#endif
-
-#ifndef    JUCE_USE_CDREADER
- #define   JUCE_USE_CDREADER 0
-#endif
-
-#ifndef    JUCE_USE_CDBURNER
- #define   JUCE_USE_CDBURNER 0
-#endif
-
-//==============================================================================
-// juce_audio_formats flags:
-
-#ifndef    JUCE_USE_FLAC
- #define   JUCE_USE_FLAC 0
-#endif
-
-#ifndef    JUCE_USE_OGGVORBIS
- #define   JUCE_USE_OGGVORBIS 0
-#endif
-
-#ifndef    JUCE_USE_MP3AUDIOFORMAT
- //#define JUCE_USE_MP3AUDIOFORMAT
-#endif
-
-#ifndef    JUCE_USE_LAME_AUDIO_FORMAT
- //#define JUCE_USE_LAME_AUDIO_FORMAT
-#endif
-
-#ifndef    JUCE_USE_WINDOWS_MEDIA_FORMAT
- //#define JUCE_USE_WINDOWS_MEDIA_FORMAT
-#endif
-
-//==============================================================================
-// juce_audio_processors flags:
-
-#ifndef    JUCE_PLUGINHOST_VST
- #define   JUCE_PLUGINHOST_VST 0
-#endif
-
-#ifndef    JUCE_PLUGINHOST_AU
- #define   JUCE_PLUGINHOST_AU 0
-#endif
-
-//==============================================================================
-// juce_core flags:
-
-#ifndef    JUCE_FORCE_DEBUG
- #define   JUCE_FORCE_DEBUG 0
-#endif
-
-#ifndef    JUCE_LOG_ASSERTIONS
- #define   JUCE_LOG_ASSERTIONS 0
-#endif
-
-#ifndef    JUCE_CHECK_MEMORY_LEAKS
- #define   JUCE_CHECK_MEMORY_LEAKS 0
-#endif
-
-#ifndef    JUCE_DONT_AUTOLINK_TO_WIN32_LIBRARIES
- //#define JUCE_DONT_AUTOLINK_TO_WIN32_LIBRARIES
-#endif
-
-//==============================================================================
-// juce_graphics flags:
-
-#ifndef    JUCE_USE_COREIMAGE_LOADER
- //#define JUCE_USE_COREIMAGE_LOADER
-#endif
-
-#ifndef    JUCE_USE_DIRECTWRITE
- //#define JUCE_USE_DIRECTWRITE
-#endif
-
-//==============================================================================
-// juce_gui_basics flags:
-
-#ifndef    JUCE_ENABLE_REPAINT_DEBUGGING
- #define   JUCE_ENABLE_REPAINT_DEBUGGING 0
-#endif
-
-#ifndef    JUCE_USE_XSHM
- #define   JUCE_USE_XSHM 0
-#endif
-
-#ifndef    JUCE_USE_XRENDER
- #define   JUCE_USE_XRENDER 1
-#endif
-
-#ifndef    JUCE_USE_XCURSOR
- #define   JUCE_USE_XCURSOR 1
-#endif
-
-//==============================================================================
-// juce_gui_extra flags:
-
-#ifndef    JUCE_WEB_BROWSER
- #define   JUCE_WEB_BROWSER 0
-#endif
-
-
-//==============================================================================
 // Audio plugin settings..
 
-#ifndef  JucePlugin_Build_VST
- #define JucePlugin_Build_VST              1
-#endif
-#ifndef  JucePlugin_Build_AU
- #define JucePlugin_Build_AU               1
-#endif
-#ifndef  JucePlugin_Build_RTAS
- #define JucePlugin_Build_RTAS             0
-#endif
-#ifndef  JucePlugin_Build_AAX
- #define JucePlugin_Build_AAX              0
-#endif
 #ifndef  JucePlugin_Name
  #define JucePlugin_Name                   "Argotlunar"
 #endif
@@ -280,5 +122,12 @@
 #ifndef  JucePlugin_AAXDisableBypass
  #define JucePlugin_AAXDisableBypass       0
 #endif
+
+#define JucePlugin_IsMidiEffect 0
+
+#define JucePlugin_LV2URI               "http://argotlunar.info/"
+#define JucePlugin_WantsLV2TimePos      1
+#define JucePlugin_WantsLV2Presets      0
+#define JucePlugin_WantsLV2State        0
 
 #endif  // __JUCE_APPCONFIG_ULIIGD__
